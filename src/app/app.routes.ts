cf9bc4b707370b1fbb66ae19dc098c08367c0503
Jon Trost <jontrost426@gmail.com>
2024-07-03 19:35:14 -0500

    Added basic styling and layout for landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import type { Routes } from "@angular/router";
+import { LandingComponent } from "./pages/landing/landing.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: "", component: LandingComponent }];

8cf7b0f5de458f0db0a7e6be3a579c0ec65b97ad
jontrost <jontrost426@gmail.com>
2024-11-21 20:23:22 -0600

    Updated to Angular 19

--- src/app/components/project-carousel/project-carousel.component.ts
+++ src/app/components/project-carousel/project-carousel.component.ts
@@ -3,11 +3,10 @@ import { PROJECT_PREVIEWS } from "../../constants/project-previews.constants";
 import type { ProjectPreview } from "../../models/project-preview.model";
 
 @Component({
-	selector: "ald-project-carousel",
-	standalone: true,
-	imports: [],
-	templateUrl: "./project-carousel.component.html",
-	styleUrl: "./project-carousel.component.scss",
+    selector: "ald-project-carousel",
+    imports: [],
+    templateUrl: "./project-carousel.component.html",
+    styleUrl: "./project-carousel.component.scss"
 })
 export class ProjectCarouselComponent {
 	projectPreviews: ProjectPreview[] = PROJECT_PREVIEWS;
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -2,10 +2,9 @@ import { Component } from "@angular/core";
 import { ProjectCarouselComponent } from "../../components/project-carousel/project-carousel.component";
 
 @Component({
-	selector: "ald-landing",
-	standalone: true,
-	imports: [ProjectCarouselComponent],
-	templateUrl: "./landing.component.html",
-	styleUrl: "./landing.component.scss",
+    selector: "ald-landing",
+    imports: [ProjectCarouselComponent],
+    templateUrl: "./landing.component.html",
+    styleUrl: "./landing.component.scss"
 })
 export class LandingComponent {}

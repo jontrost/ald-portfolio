d7fc8aae0e304c5c4a26c9c3d505c1bbb6882d43
jontrost <jontrost426@gmail.com>
2024-11-21 21:29:57 -0600

    Created initial carousel animation

--- src/app/constants/project-previews.constants.ts
+++ src/app/constants/project-previews.constants.ts
@@ -1,18 +1,26 @@
 export const PROJECT_PREVIEWS = [
 	{
 		imageSrc: "/assets/placeholder-image.jpg",
-		description: "Brief project summary text.",
+		description: "Brief project summary text. (1)",
 	},
 	{
 		imageSrc: "/assets/placeholder-image.jpg",
-		description: "Brief project summary text.",
+		description: "Brief project summary text. (2)",
 	},
 	{
 		imageSrc: "/assets/placeholder-image.jpg",
-		description: "Brief project summary text.",
+		description: "Brief project summary text. (3)",
 	},
 	{
 		imageSrc: "/assets/placeholder-image.jpg",
-		description: "Brief project summary text.",
+		description: "Brief project summary text. (4)",
+	},
+    {
+		imageSrc: "/assets/placeholder-image.jpg",
+		description: "Brief project summary text. (5)",
+	},
+    {
+		imageSrc: "/assets/placeholder-image.jpg",
+		description: "Brief project summary text. (6)",
 	},
 ];

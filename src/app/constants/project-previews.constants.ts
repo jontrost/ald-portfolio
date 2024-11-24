47da1c75bfd033eb6a967d6a16eacc00adc27546
jontrost <jontrost426@gmail.com>
2024-11-23 18:58:21 -0600

    Added accessibility outlines and ability to select previews

--- src/app/constants/project-previews.constants.ts
+++ src/app/constants/project-previews.constants.ts
@@ -4,7 +4,7 @@ export const PROJECT_PREVIEWS = [
 		description: "Brief project summary text. (1)",
 	},
 	{
-		imageSrc: "/assets/placeholder-image.jpg",
+		imageSrc: "/assets/placeholder-image-alternate.jpg",
 		description: "Brief project summary text. (2)",
 	},
 	{
@@ -12,15 +12,15 @@ export const PROJECT_PREVIEWS = [
 		description: "Brief project summary text. (3)",
 	},
 	{
-		imageSrc: "/assets/placeholder-image.jpg",
+		imageSrc: "/assets/placeholder-image-alternate.jpg",
 		description: "Brief project summary text. (4)",
 	},
-    {
+	{
 		imageSrc: "/assets/placeholder-image.jpg",
 		description: "Brief project summary text. (5)",
 	},
-    {
-		imageSrc: "/assets/placeholder-image.jpg",
+	{
+		imageSrc: "/assets/placeholder-image-alternate.jpg",
 		description: "Brief project summary text. (6)",
 	},
 ];
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,15 +1,17 @@
-import { Component } from "@angular/core";
-import { ProjectMenuComponent } from "../../components/project-menu/project-menu.component";
+import { Component, signal } from "@angular/core";
 import { PROJECT_PREVIEWS } from "../../constants/project-previews.constants";
 import type { ProjectPreview } from "../../models/project-preview.model";
 
 @Component({
 	selector: "ald-landing",
-	imports: [ProjectMenuComponent],
 	templateUrl: "./landing.component.html",
 	styleUrl: "./landing.component.scss",
 })
 export class LandingComponent {
-	// Restructure this so that these project previews are not duplicated between here and the project menu
-	projectPreviews: ProjectPreview[] = PROJECT_PREVIEWS;
+	readonly projectPreviews: ProjectPreview[] = PROJECT_PREVIEWS;
+	selectedProjectPreviewIndex = signal<number>(0);
+
+	selectProjectPreview(index: number) {
+		this.selectedProjectPreviewIndex.set(index);
+	}
 }
